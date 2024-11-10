36084c1bd5bf8a80e106c6be340e3f0c7ef97a35
piyush-gangrade <piyushgangrade750@gmail.com>
2024-11-10 12:01:49 +0530

    fix axios default base url

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -10,27 +10,27 @@ interface UpdateTask extends NewTask {
     status: string
 }
 
-const baseURL: string = import.meta.env.VITE_BASE_URL + "/tasks";
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const getAllTasks = ()=>{
-    return axios.get(`${baseURL}`)
+    return axios.get("/tasks")
 }
 
 const getTask = (id: string)=>{
-    return axios.get(`${baseURL}/${id}`)
+    return axios.get(`/tasks/${id}`)
 }
 
 const addNewTask = (taskDetails: NewTask)=>{
-    return axios.post(`${baseURL}`,taskDetails)
+    return axios.post("/tasks",taskDetails)
 }
 
 
 const updateTask = (id: string, taskDetails: UpdateTask)=>{
-    return axios.put(`${baseURL}/${id}`, taskDetails)
+    return axios.put(`/tasks/${id}`, taskDetails)
 }
 
 const deleteTask = (id: string)=>{
-    return axios.delete(`${baseURL}/${id}`)
+    return axios.delete(`/tasks/${id}`)
 }
 
 
