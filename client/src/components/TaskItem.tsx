d7ba75ffff8288f7e2c00afd5ce51b34cbbaae24
piyush-gangrade <piyushgangrade750@gmail.com>
2024-11-10 21:54:15 +0530

    add drag and drop functionality

--- client/src/components/TaskItem.tsx
+++ client/src/components/TaskItem.tsx
@@ -1,10 +1,14 @@
 import { Task } from "../types/Task";
 
-export default function TaskItem({title, description, deadline}: Task){
+export default function TaskItem({_id, title, description, deadline}: Task){
     const deadlineDate = new Date(deadline);
 
+    const handleOnDrag = (e: React.DragEvent, _id: string)=>{
+        e.dataTransfer.setData("taskId", _id);
+    }
+
     return (
-        <div className="task--item">
+        <div className="task--item" draggable onDragStart={(e)=> handleOnDrag(e, _id)}>
             <div className="task--header">
                 <div className="task--priority">Low</div>
                 <div className="task--option">...</div>
--- client/src/components/TaskList.tsx
+++ client/src/components/TaskList.tsx
@@ -1,5 +1,6 @@
 import TaskItem from "./TaskItem"
 import { useTask } from "../context/TaskContext"
+import { updateTask } from "../api";
 
 interface TaskListType {
     heading: string
@@ -7,10 +8,48 @@ interface TaskListType {
 }
 
 export default function TaskList({heading, status}: TaskListType){
-    const {tasks} = useTask();
+    const {tasks, setTasks} = useTask();
+
+    const handleOnDrop = async(e: React.DragEvent)=>{
+        const taskId: string = e.dataTransfer.getData("taskId");
+        const dragTask = tasks.find(task => task._id === taskId);
+        
+        if(!dragTask){
+            return;
+        }
+        try {
+            const res = await updateTask(taskId, {
+                title: dragTask.title, 
+                description: dragTask.description,
+                deadline: dragTask.deadline,
+                status: status
+            })
+
+            if(res?.data?.success){
+                setTasks(tasks => tasks.map(task => {
+                    if(task._id === taskId){
+                        return {
+                            ...task,
+                            status: status
+                        }
+                    }
+                    else{
+                        return task
+                    }
+                }))
+            }
+        } 
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleDragOver = (e: React.DragEvent)=>{
+        e.preventDefault();
+    }
 
     return (
-        <div className="task--list">
+        <div className="task--list" onDrop={handleOnDrop} onDragOver={handleDragOver}>
             <div className="task--list--heading">{heading}</div>
             {
                 tasks
