e6125eaec89c695e2cb0cfa578f5b6c47631b986
piyush-gangrade <piyushgangrade130@gmail.com>
2025-02-20 17:36:14 +0530

    add login controller

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -11,9 +11,11 @@ const registre = asyncHandler(async(req: Request, res: Response)=>{
         throw new AppError(404, "username, email or password is not exist.");
     }
 
-    const existUser = await User.findOne({username});
+    const existUser = await User.findOne({
+        $or: [{username}, [email]]
+    });
     if(existUser){
-        throw new AppError(409, "username already exist.")
+        throw new AppError(409, "user already exist.")
     }
 
     const hashPassword = await authService.hashedPassword(password);
@@ -39,7 +41,41 @@ const registre = asyncHandler(async(req: Request, res: Response)=>{
     return;
 })
 
+const login = asyncHandler(async(req: Request, res: Response)=>{
+    const {username, email, password} = req.body;
+    if(!username && !email){
+        throw new AppError(404, "username or email is required.");
+    }
+
+    if(!password){
+        throw new AppError(404, "password is required.");
+    }
+
+    const user = await User.findOne({
+        $or: [{username}, {email}]
+    });
+    if(!user){
+        throw new AppError(409, "user is not exist.")
+    }
+
+    const {accessToken, refreshToken} = authService.generateAccessAndRefreshTokens({id: user._id, username: user.username});
+
+    user.refreshToken = refreshToken;
+    await user.save();
+    res
+        .status(201)
+        .cookie("accessToken", accessToken)
+        .cookie("refreshToken", refreshToken)
+        .json({
+            response: {id: user._id},
+            message: "User login successfully",
+            success: true
+        })
+    return;
+})
+
 
 export {
-    registre
+    registre,
+    login
 }
--- server/src/routers/auth.router.ts
+++ server/src/routers/auth.router.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { registre } from "../controllers/auth.controller";
+import { login, registre } from "../controllers/auth.controller";
 
 const router = Router();
 
 router.post("/register", registre);
+router.post("/login", login);
 
 export default router;
